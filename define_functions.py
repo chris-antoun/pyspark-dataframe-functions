479dba2a5d54b4b45e7524c913d13e4a02ee584d
Chris Antoun <38592009+chris-antoun@users.noreply.github.com>
2021-02-09 14:25:57 +0100

    Adding new function
    Adding a function called join_on_closest_time_match. This function takes two PySpark dataframes and joins them based on the closest time match between two timestamp columns.

--- define_functions.py
+++ define_functions.py
@@ -30,3 +30,103 @@ def get_column_list_of_data_type(dataframe: DataFrame, data_type) -> list:
     except TypeError:
         print(f"The specified type {data_type} is not valid.")
         return []
+    
+ def join_on_closest_time_match(
+        primary_dataframe: DataFrame,
+        secondary_dataframe: DataFrame,
+        primary_time_column: str,
+        secondary_time_column: str,
+        additional_join_columns=[],
+        max_seconds_difference_tolerance: int = 3600,
+    ) -> DataFrame:
+
+        """This function takes two dataframes containing date columns. It joins them based on the closest timestamp match, such that the difference between the two matched timestamps is at most a certain value (given by the max_time_difference_tolerance parameter).
+        primary_dataframe: DataFrame (The dataframe containing the original timestamp column)
+        secondary_dataframe: DataFrame (The dataframe containing timestamp column)
+        primary_time_column: str (A string representing the name of the primary_dataframe's timestamp column)
+        secondary_time_column: str (A string representing the name of the secondary_dataframe's timestamp column)
+        additional_join_columns: list (A list of strings representing columns to join the dataframes on, besides the closest timestamp match)
+        max_seconds_difference_tolerance: int (An integer representing the threshold at which two timestamps can be considered sufficiently close)
+        
+        Example usage:
+        primary_dataframe = join_on_closest_time_match(primary_dataframe, secondary_dataframe, "Timestamp", "Timestamp_secondary", ["CategoryCode", "GroupId"], 2*60*60);
+        """
+
+        # Step 0: Check that the input data has expected format
+        condition1 = (
+            primary_dataframe.select(primary_time_column).dtypes[0][1] == "timestamp"
+        )
+        condition2 = (
+            secondary_dataframe.select(secondary_time_column).dtypes[0][1]
+            == "timestamp"
+        )
+        if (condition1 and condition2) == False:
+            print(
+                "Datatype of the relevant timecolumns in the passed in DataFrames is not 'timestamp'"
+            )  # This should be error logging
+            return None
+
+        # Step 1: Select needed columns
+        primary_dataframe_selection = primary_dataframe.select(
+            primary_time_column, *additional_join_columns
+        ).dropDuplicates()
+        secondary_dataframe_selection = secondary_dataframe.select(
+            secondary_time_column, *additional_join_columns
+        ).dropDuplicates()
+
+        # Step 2: Join on additional column if specified. Otherwise, do a cross join
+        if len(additional_join_columns) != 0:
+            print(f"Joining the tables on {additional_join_columns}")
+            helper = primary_dataframe_selection.join(
+                secondary_dataframe_selection, additional_join_columns, how="left"
+            ).dropDuplicates()
+        else:
+            print(
+                f"No specified join columns. Cross join between tables of row count {primary_dataframe_selection.count()} and {secondary_dataframe_selection.count()} will be implemented."
+            )
+            helper = primary_dataframe_selection.crossJoin(
+                secondary_dataframe_selection
+            )
+
+        print(f"The size of the first joined dataframe is {helper.count()}")
+        # Step 3: Calculate the difference in time between the two dates
+        helper = helper.withColumn(
+            "TimeDifference",
+            abs(
+                col(primary_time_column).cast("float")
+                - col(secondary_time_column).cast("float")
+            ),
+        )
+
+        # Step 4: Remove matched rows where the time difference is greater than tolerated level
+        helper = helper.filter(
+            col("TimeDifference") <= max_seconds_difference_tolerance
+        )
+
+        # Step 5: Sort the dataframe in ascending order based on the difference in time, i.e. the "first" record is the one closest to the time in primary dataframe (If two timestamps are equally distant, we are then sorting by that column in ascending order to always pick the "older" timestamp)
+        helper = helper.sort(
+            *additional_join_columns,
+            col(primary_time_column),
+            col("TimeDifference").asc(),
+            col(secondary_time_column).asc(),
+        )
+
+        # Step 6: Select the closest time
+        helper = helper.groupby(*additional_join_columns, primary_time_column).agg(
+            first(col(secondary_time_column)).alias(secondary_time_column),
+            first(col("TimeDifference")).alias("TimeDifference"),
+        )
+
+        # Step 7: Join with primary dataframe
+        primary_dataframe = primary_dataframe.join(
+            helper, [*additional_join_columns, primary_time_column], how="left"
+        )
+
+        # Step 8: Join with secondary dataframe
+        primary_dataframe = primary_dataframe.join(
+            secondary_dataframe,
+            [*additional_join_columns, secondary_time_column],
+            how="left",
+        ).drop(secondary_time_column, "TimeDifference")
+
+        return primary_dataframe
